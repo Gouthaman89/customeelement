35bbf3c96e6668683d7dd7194e41b879a771420b
Gouthaman89 <42800698+Gouthaman89@users.noreply.github.com>
2022-03-29 09:56:18 +0800

    Add files via upload

---
+++ docs/3dbuilding.js
@@ -0,0 +1,51 @@
+(function () {
+    class FGANTT extends HTMLElement {
+      constructor() {
+        super();
+        this.innerHTML =`<canvas  id="myCanvas" width="600" height="650"></canvas>`;
+
+      }
+      connectedCallback() {
+        var shadow =document.getElementsByTagName('com-demo-gauge');
+        // var custelem = shadow.host;
+        // this.$width = custelem.parentNode.parentNode.parentNode.style.width;
+         //this.$height = custelem.parentNode.parentNode.parentNode.style.height;
+         let LoadLibs = async function(host, data, props) {
+             try {
+                 //await host.loadScript("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js", shadow);
+               //await host.loadScript("./dist/xeokit-sdk.min.es.js", shadow);
+                  await host.loadScript1("https://gouthaman89.github.io/customeelement/docs/moduel2.js", shadow);
+             } catch (e) {
+                 console.log(JSON.stringify(e));
+             } finally {
+               host.drawChart(data, props);
+             }
+         };
+         LoadLibs(this, this.$data, this._props);
+        this._init = false;
+      }
+      drawChart(value, config) {
+        //import {Viewer, XKTLoaderPlugin, ContextMenu, TreeViewPlugin, NavCubePlugin, AnnotationsPlugin} from "./dist/xeokit-sdk.min.es.js";
+      var rr=this.shadowRoot;
+    }
+            
+
+loadScript1(src, shadowRoot) {
+    return new Promise(function(resolve, reject) {
+        let script = document.createElement('script');
+        script.src = src;
+        script.type="module";
+        script.crossOrigin = '*';
+        script.onload = () => {
+            mode: "cors",
+            console.log("Load: " + src);
+            resolve(script);
+        };
+        script.onerror = () => reject(new Error(`Script load error for ${src}`));
+        document.body.appendChild(script);
+    });
+}        
+    }
+
+    customElements.define("com-demo-gauge", FGANTT);
+  })();
