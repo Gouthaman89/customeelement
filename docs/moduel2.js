53995f037e5f00a8f4cbbcbb2215d88f747c605b
Gouthaman89 <42800698+Gouthaman89@users.noreply.github.com>
2022-03-29 13:10:40 +0800

    Add files via upload

---
+++ docs/moduel2.js
@@ -0,0 +1,32 @@
+ //------------------------------------------------------------------------------------------------------------------
+    // Import the modules we need for this example
+    //------------------------------------------------------------------------------------------------------------------
+
+    import {Viewer, XKTLoaderPlugin} from "https://gouthaman89.github.io/customeelement/docs/xeokit-sdk.min.es.js";
+
+    //------------------------------------------------------------------------------------------------------------------
+    // Create a Viewer, arrange the camera
+    //------------------------------------------------------------------------------------------------------------------
+
+    const viewer = new Viewer({
+        canvasId: "myCanvas",
+        transparent: true
+    });
+
+    //----------------------------------------------------------------------------------------------------------------------
+    // Load a model
+    //----------------------------------------------------------------------------------------------------------------------
+
+    const xktLoader = new XKTLoaderPlugin(viewer);
+
+    const model = xktLoader.load({
+        id: "myModel",
+        src: "https://gouthaman89.github.io/customeelement/docs/BH220211216.ifc.xkt",
+        //metaModelSrc: "../assets/models/xkt/v7/MAP/MAP.json",
+        edges: true,
+        excludeUnclassifiedObjects: false
+    });
+    
+    model.on("loaded", function () {        
+        viewer.cameraFlight.jumpTo(model);
+    });
